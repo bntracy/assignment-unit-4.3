a3c192de4be73cfd101eccb15741a28e96a01550
Beth Tracy <bntracy@gmail.com>
2024-01-05 16:45:54 -0600

    required items complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,15 +1,41 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+function addItem(item) {
+    basket.push(item);
+    return true;
+} // end addItem function
 
+// testing
+console.log('Original basket, should be empty:', basket);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Basket is now:', basket);
 
+function listItems() {
+    for (let basketItem of basket) {
+        console.log(basketItem);
+    } // end for
+} // end listItems function
 
+// testing
+listItems(); // should log apples
+// add some more items to test
+addItem('bananas');
+addItem('pears');
+listItems(); // should log apples, bananas, pears on separate lines
 
+function empty() {
+    while (basket.length > 0) {
+        basket.pop();
+    }
+} // end empty function
 
-
-
-
+// testing
+console.log('Now to empty the basket:');
+empty();
+console.log('Basket, should be empty again:', basket);
 
 
 // DO NOT MODIFY
